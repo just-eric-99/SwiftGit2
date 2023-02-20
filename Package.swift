c26adee23bc452b21b3d0e963cecd34110c4c266
Eric Low <64390777+just-eric-99@users.noreply.github.com>
2023-02-20 19:18:34 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGit2",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
